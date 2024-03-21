8536aa7eb9c6b67cb69d883f64f491ec91a6f56e
DAVENJAGI <njagidave39@gmail.com>
2024-03-21 23:35:55 +0300

    Complex_DB

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
 -- Script that creates Table that users with below requirement
-CREATE TABLE IF NOT EXIST users (
-	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
-	email VARCHAR(255) NOT NULL UNIQUE
-	name VARCHAR(255)
-	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country VARCHAR(2) NOT NULL DEFAULT 'US',
+	CHECK (country IN ('US', 'CO', 'IN'))
 );
