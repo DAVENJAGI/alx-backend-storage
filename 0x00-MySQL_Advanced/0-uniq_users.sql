8f957c3509aa8e2af24271210accc9a143a53761
DAVENJAGI <njagidave39@gmail.com>
2024-03-21 23:09:31 +0300

    Complex_DB

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,9 @@
 -- A script that creates table users with attribute id, email, name
-
+-- With attributes ID, integer, never null, auto increment
+-- email, varchar, never null, and unique
+-- script to execute if table exists
 CREATE TABLE IF NOT EXIST user(
-	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 );
