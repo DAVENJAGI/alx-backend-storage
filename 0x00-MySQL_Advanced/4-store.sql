dcdc1f66560620616d17db5b01bd91d069f9af7a
DAVENJAGI <njagidave39@gmail.com>
2024-03-23 14:55:38 +0300

    Advanced_SQL

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,12 +1,3 @@
 -- Script that creates a trigger that decreases quantity of an item after a pending order
-DROP TRIGGER IF EXISTS reduce_quantity
-DELIMETER $$
-CREATE TRIGGER reduce_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-	UPDATE items
-	SET quantity = quantity - NEW.number
-	WHERE name = NEW.item_name
-END $$
-DELIMETER ;
+CREATE TRIGGER reduce_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
