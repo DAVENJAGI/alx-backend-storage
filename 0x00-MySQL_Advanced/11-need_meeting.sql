eefe764a5c58a7bdabf5e5090103703fd7a5d891
DAVENJAGI <njagidave39@gmail.com>
2024-03-23 16:51:44 +0300

    Advanced_SQL

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,11 @@
+-- Creates view need_meeting that lists students that have less than 80
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+    SELECT name
+        FROM students
+        WHERE score < 80 AND
+            (
+                last_meeting IS NULL
+                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
+            )
+;
