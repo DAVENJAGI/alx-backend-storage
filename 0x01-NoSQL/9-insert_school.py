4e76b6bdf35e099b38fe6b637a3f27c9ba5bff8f
DAVENJAGI <njagidave39@gmail.com>
2024-03-26 12:53:43 +0300

    NoSQL

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/python3
+"""
+Insert a new document
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    '''inserts new document in a collection'''
+    newDocu = mongo_collection(insertOne(kwargs))
+    return newDocu.insertedId
