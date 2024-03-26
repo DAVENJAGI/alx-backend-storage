73a1d7dda772aade937a85b998a4fe9ed6964039
DAVENJAGI <njagidave39@gmail.com>
2024-03-26 13:02:25 +0300

    NoSQL

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,5 +5,5 @@ Changes all topics of school document
 
 
 def update_topics(mongo_collection, name, topics):
-    '''Updates topics'''
-    mongo_collection.update_any({'name': name}, {$set: {'topics': topics}})
+    """Updates topics based on school name"""
+    mongo_collection.updateMany({'name': name}, {$set: {'topics': topics}})
