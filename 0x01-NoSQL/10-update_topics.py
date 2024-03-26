35f1692687b52bbafc06794ce2af415f8a4426fc
DAVENJAGI <njagidave39@gmail.com>
2024-03-26 13:07:03 +0300

    NoSQL

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ Changes school document based on the name
 
 def update_topics(mongo_collection, name, topics):
     """Updates topics based on school name"""
-    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
