8c34bd0c91bd0c266c51bfb24d9c2d151790d47d
DAVENJAGI <njagidave39@gmail.com>
2024-03-26 13:50:43 +0300

    NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+A function that returns all students sorted by avg score
+"""
+
+
+def top_students(mongo_collection):
+    '''
+    The top must be sorted
+    '''
+    return mongo_collectin.aggregate([
+        {"$project": {
+            "name": "$name",
+            "average_score": {"$avg": "$topics.score"}
+        }}
+        {"$sort": {"average_score": -1}}
+    ])
