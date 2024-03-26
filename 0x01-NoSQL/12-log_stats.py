5cb74aa09ea140ba7fca7bf78e51e898051f89c7
DAVENJAGI <njagidave39@gmail.com>
2024-03-26 13:52:48 +0300

    NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,15 +7,15 @@ stored in MongoDB
 
 def log_stats():
     """log stats of nginx"""
-    client = MongoClient("mongodb://127.0.0.1:27017")
+    client = MongoClient('mongodb://127.0.0.1:27017')
     logCollection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
+    total = logsiCollection.count_documents({})
+    get = logsCollection.count_documents({"method": "GET"})
+    post = logsCollection.count_documents({"method": "POST"})
+    put = logsCollection.count_documents({"method": "PUT"})
+    patch = logsCollection.count_documents({"method": "PATCH"})
+    delete = logsCollection.count_documents({"method": "DELETE"})
+    path = logsCollection.count_documents(
         {"method": "GET", "path": "/status"})
     print(f"{total} logs")
     print("Methods:")
