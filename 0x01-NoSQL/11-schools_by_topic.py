35ffc187e1ac0e33401e0b0ecda675f941899e0e
DAVENJAGI <njagidave39@gmail.com>
2024-03-26 13:11:06 +0300

    NoSQL

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+A function that returns a list of school having group
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns list of school"""
+    return mongo_collection.find("topics": topic})
