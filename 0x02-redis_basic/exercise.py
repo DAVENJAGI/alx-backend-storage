fb10617b8c2f2fd648bc3f9aaa042ec7d706b544
DAVENJAGI <njagidave39@gmail.com>
2024-03-28 03:42:18 +0300

    Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Create cache class. In the __init__ method, stores an instance
+"""
+from redis import Redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    '''class cache'''
+    def __init__(self):
+        '''initialize method'''
+        self.__redis = Redis()
+        self.__redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        '''generates a random key, stores the data in redis
+        using random key and returns the key'''
+
+        key = str(uuid.uuid4())
+        self.__redis.set(key, data)
+        return key
